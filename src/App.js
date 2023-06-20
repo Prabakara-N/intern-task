66498c923287ab6d3a2d076f36c563cd6cd235c4
Prabakaran-M <mspk028@gmail.com>
2023-06-20 18:28:33 +0530

    project-initial-setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Categories from "./components/Categories";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div>
+      <Categories />
+    </div>
+  );
+}
+
+export default App;
